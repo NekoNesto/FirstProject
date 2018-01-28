44b7f06c7930434c08c89b9b12dc50ded556b63a
NekoNesto <neko.nesto.neko@outlook.com>
2018-01-28 22:10:33 +0100

    Program file dodat: moguce prepravke

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+using Vjezba;
+namespace AppVjezbaConsole02
+{ 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Kupac k = new Kupac(); 
+        
+        }
+    }
+}
