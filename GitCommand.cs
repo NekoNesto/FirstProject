f5d9c1b9b340109e667eeac45215fac5d2b23c7b
NekoNesto <neko.nesto.neko@outlook.com>
2018-01-28 23:17:04 +0100

    Ovo su komande za git vjezbu

---
+++ GitCommand.cs
@@ -0,0 +1,31 @@
+1  ls
+    2  cd ~
+    3  pwd
+    4  ls
+    5  cd Documents
+    6  clear
+    7  clear
+    8  git config --global user.name "HarisBegluk"
+    9  git config --global user.email "haris.begluk.webprogramming@outlook.com"
+   10  ls
+   11  ls -la
+   12  mkdir GIT
+   13  cd GIT
+   14  ls
+   15  git clone ??https://github.com/NekoNesto/FirstProject.git
+   16  git add .
+   17  git clone ??https://github.com/NekoNesto/FirstProject.git
+   18  git clone https://github.com/NekoNesto/FirstProject.git
+   19  cd FirstProject/
+   20  clear
+   21  git add .
+   22  git status
+   23  git add .
+   24  git status
+   25  git add Program01.cs
+   26  git status
+   27  git commit -m "Novi program Program01.cs"
+   28  git status
+   29  git push -u origin master
+   30  clear
+   31  history
