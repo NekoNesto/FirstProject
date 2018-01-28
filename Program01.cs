b16076583fc9b3255cd08c2bf05430087b0cebd7
HarisBegluk <haris.begluk.webprogramming@outlook.com>
2018-01-28 22:53:55 +0100

    Novi program Program01.cs

---
+++ Program01.cs
@@ -0,0 +1,16 @@
+
+
+using System;
+using Vjezba;
+namespace AppVjezbaConsole02
+{ 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              System.Console.WriteLine("Ovo je novi program!!!");
+        
+        }
+    }
+}
